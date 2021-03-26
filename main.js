3e560106f5aba50da5ab71c362e947c076b83fee
mattdavis0351 <mattdavis0351@github.com>
2021-03-26 11:45:38 -0700

    turning feeback into class for more control

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const { Feedback } = require("./lib/feedback");
 
 async function run() {
   try {
     // {
-    //   report: [
+    //   reports: [
     //     {
     //       filename: "the filename associated with the report",
     //       isCorrect: true,
@@ -32,23 +33,38 @@ async function run() {
     const fb = core.getInput("feedback");
     if (!fb) return;
 
-    const report = JSON.parse(fb);
+    const { reports } = JSON.parse(fb);
 
     const token = core.getInput("github-token");
     const octokit = github.getOctokit(token);
+    const context = github.context;
+    const feedback = new Feedback(octokit, context, ...reports);
 
-    if (report.type !== "actions") {
-      // decide how to display feedback based on paylaod from reort.type
-      const res = await octokit.issues.create({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        title: "Oh no!",
-        labels: ["bug"],
-        body: report.msg,
-      });
-
-      return;
+    for (const report of feedback.reports) {
+      switch (report.display_type) {
+        case "issues":
+          feedback.provideFeebackUsingIssues(report);
+          break;
+        // case "pull_requests":
+        //   feedback.createPullRequests(octokit, report.msg);
+        //   break;
+        default:
+          console.log("default case");
+          break;
+      }
     }
+    // if (report.type !== "actions") {
+    //   // decide how to display feedback based on paylaod from reort.type
+    //   const res = await octokit.issues.create({
+    //     owner: github.context.repo.owner,
+    //     repo: github.context.repo.repo,
+    //     title: "Oh no!",
+    //     labels: ["bug"],
+    //     body: report.msg,
+    //   });
+
+    //   return;
+    // }
     if (report.level === "warning" || report.level === "fatal") {
       core.warning(report.msg);
     } else {
