f14bf147ca6170fee8a26a1caefed6fd7d0e2cf5
mattdavis0351 <mattdavis0351@github.com>
2021-03-26 10:03:40 -0700

    adding signature comment for easy reference

--- main.js
+++ main.js
@@ -3,6 +3,32 @@ const github = require("@actions/github");
 
 async function run() {
   try {
+    // {
+    //   report: [
+    //     {
+    //       filename: "the filename associated with the report",
+    //       isCorrect: true,
+    //       level: "info",
+    //       display_type: "actions",
+    //       msg: "the message",
+    //       error: {
+    //         expected: "the expected string",
+    //         got: "the gotten string",
+    //       },
+    //     },
+    //     {
+    //       filename: "",
+    //       isCorrect: false,
+    //       display_type: "issues",
+    //       level: "fatal",
+    //       msg: "the message",
+    //       error: {
+    //         expected: "",
+    //         got: "",
+    //       },
+    //     },
+    //   ];
+    // }
     const fb = core.getInput("feedback");
     if (!fb) return;
 
